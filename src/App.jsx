bdbf117628b12b9fc84b00797ffcb8228cb85939
helloYash112 <rajputyashwardhan17747@gmail.com>
2024-10-16 14:50:06 +0530

    completed this project

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,56 @@ import {Player} from './components/Player'
 import { GameBoard } from './components/GameBoard'
 import { useState } from 'react'
 import { Log } from './components/Log';
+import { WINNING_COMBINATIONS } from './components/WINNING_COMBINATION.JS';
+import Result from './components/Result';
+const plrBrd=[[null,null,null],[null,null,null],[null,null,null]];
+
+
 function App() {
-  const[activePlr,setActivePlr]=useState('X');
+  
   const[preArr,udtArr]=useState([]);
+  let winning;
+  let intialGameBoard= [...plrBrd.map(innerArr=> [...innerArr])];
+  
+  for(const data of preArr){
+     const{squre,symbol}=data;
+     const{row,col}=squre;
+     intialGameBoard[row][col]=symbol;
+  }
+
+  for(const combination of WINNING_COMBINATIONS){
+   const fps=intialGameBoard[combination[0].row][combination[0].column];
+   const sps=intialGameBoard[combination[1].row][combination[1].column];
+   const tps=intialGameBoard[combination[2].row][combination[2].column];
+   
+   
+   if(fps && fps===sps && fps===tps){
+      winning=fps;
+   }
+  }
+ const draw=(preArr.length== 9 && !winning);
+
+  const activePlr=setPlrSym(preArr);
+  function setPlrSym(preArr){
+   let smbl = 'X';
+   if( preArr.length>0 && preArr[0].symbol=='X'){
+      smbl='O';
+   }
+      return smbl;
+  }
   function handleActivePlayer(rowIdx,colIdx){
-   console.log('u called active player');
-   setActivePlr((plr)=>plr== 'X' ? 'O' : 'X');
    
    udtArr(oldArr=>{
-      let smbl = 'X';
-      if( oldArr.length>0 && oldArr[0].symbol=='X'){
-         smbl='O';
-      }
+     
+        const smbl=setPlrSym(oldArr);
       const newArr=[{squre:{row :rowIdx ,col :colIdx}, symbol :smbl},...oldArr];
       return newArr;
 
    })
   }
+  function rematch(){
+      udtArr([]);
+     }
 
   return (<main>
    <div id="game-container">
@@ -26,7 +59,8 @@ function App() {
        <Player name='player 1' symbol='X' isValid={activePlr=='X'}/> 
        <Player name='player 2' symbol='O' isValid={activePlr=='O'} />    
     </ol>
-    <GameBoard showActivePlr={handleActivePlayer} turn={preArr}/>
+    {(winning || draw) && <Result winner={winning} match={rematch} />}
+    <GameBoard showActivePlr={handleActivePlayer} bord={intialGameBoard}/>
     </div>
     
     <Log turn={preArr} />
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,19 +1,14 @@
 
-const plrBrd=[[null,null,null],[null,null,null],[null,null,null]];
-export function GameBoard({showActivePlr,turn}){
-let preArr=plrBrd;
- for(const data of turn){
-    const{squre,symbol}=data;
-    const{row,col}=squre;
-    preArr[row][col]=symbol;
- }
+
+export function GameBoard({showActivePlr,bord}){
+
   
     return <ol id="game-board">
         {
-            preArr.map((row,rowIdx)=><li key={rowIdx}>
+            bord.map((row,rowIdx)=><li key={rowIdx}>
              <ol>
               {
-                row.map((plrSbl,colIdx)=><li key={colIdx}><button onClick={()=>showActivePlr(rowIdx,colIdx)}>{plrSbl}</button></li>)
+                row.map((plrSbl,colIdx)=><li key={colIdx}><button onClick={()=>showActivePlr(rowIdx,colIdx)} disabled={plrSbl !=null}>{plrSbl}</button></li>)
               }
              </ol>
             </li>
